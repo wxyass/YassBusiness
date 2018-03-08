b4dd688c1422c1059c39408cfb8d4db93408f963
wxyass <wxyass@gmail.com>
2018-03-08 20:44:57 +0800

    格式化代码

--- yasssdk/src/main/java/com/yass/okhttp/listener/DisposeDataHandle.java
+++ yasssdk/src/main/java/com/yass/okhttp/listener/DisposeDataHandle.java
@@ -3,26 +3,22 @@ package com.yass.okhttp.listener;
 /**
  * @author yass
  */
-public class DisposeDataHandle
-{
-	public DisposeDataListener mListener = null;
-	public Class<?> mClass = null;
-	public String mSource = null;
+public class DisposeDataHandle {
+    public DisposeDataListener mListener = null;
+    public Class<?> mClass = null;
+    public String mSource = null;
 
-	public DisposeDataHandle(DisposeDataListener listener)
-	{
-		this.mListener = listener;
-	}
+    public DisposeDataHandle(DisposeDataListener listener) {
+        this.mListener = listener;
+    }
 
-	public DisposeDataHandle(DisposeDataListener listener, Class<?> clazz)
-	{
-		this.mListener = listener;
-		this.mClass = clazz;
-	}
+    public DisposeDataHandle(DisposeDataListener listener, Class<?> clazz) {
+        this.mListener = listener;
+        this.mClass = clazz;
+    }
 
-	public DisposeDataHandle(DisposeDataListener listener, String source)
-	{
-		this.mListener = listener;
-		this.mSource = source;
-	}
+    public DisposeDataHandle(DisposeDataListener listener, String source) {
+        this.mListener = listener;
+        this.mSource = source;
+    }
 }
